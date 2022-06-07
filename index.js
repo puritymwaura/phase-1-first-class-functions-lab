20c504a39777d4e3b5f5f7859e29a3d2a08de749
Purity Mwaura <purity.mwaura@student.moringaschool.com>
2022-06-07 15:58:20 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+
+const returnFirstTwoDrivers = (arr) => arr.slice(0, 2)
+
+const returnLastTwoDrivers = (arr) => arr.slice(arr.length-2)
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+let createFareMultiplier = (mult) => ((fare) => fare * mult)
+
+let  fareDoubler = (fare) => createFareMultiplier(2)(fare);
+
+let fareTripler = (fare) => createFareMultiplier(3)(fare);
+
+
+function selectDifferentDrivers(arr, func){
+
+    if (func === returnFirstTwoDrivers){
+
+        return returnFirstTwoDrivers(arr)
+
+    }else if(func === returnLastTwoDrivers){
+
+        return returnLastTwoDrivers(arr)
+    }
+}
